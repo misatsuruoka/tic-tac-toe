c31c94b2ca20d2ca44ca43f7626122d462f52b78
misatsuruoka <misa.0429@docomo.ne.jp>
2022-12-10 17:30:24 +0900

    Fix: ボタンが表示されないバグを修正

--- src/index.js
+++ src/index.js
@@ -113,7 +113,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
